6ce0e6557a732777b2733d0f5a31f7ef61d557c2
Abhi Mishra <sonusamda@gmail.com>
2024-07-04 15:51:12 +0530

    Mongo connect

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express"
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO
+  )
+  .then(() => {
+    console.log("MongoDb  Connected Successfully !!!");
+  })
+  .catch((err) => {
+    console.log("MongoDb  Connection Failed !!", err);
+  });
+
 
 
 const app = express();
