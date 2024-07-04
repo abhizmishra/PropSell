b784d7465da2d970dd752894e94d01a0e9e49131
Abhi Mishra <sonusamda@gmail.com>
2024-07-04 14:55:24 +0530

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import {FaSearch} from "react-icons/fa"
 const Header = () => {
   return (
     <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+      <div className="flex justify-evenly items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Sahand</span>
