a5cdda0e160bdb56fe42efc625238cd6ec8429b3
Abhi Mishra <sonusamda@gmail.com>
2024-07-06 12:19:34 +0530

    Signup page create

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>
-      Sign 
-    </div>
-  )
-}
+    <>
+      <div className=" p-3 max-w-md mx-auto">
+        <h1 className="text-center my-7 text-3xl font-semibold">Sign Up</h1>
 
-export default SignUp
+        <form className="flex flex-col gap-3 ">
+          <input
+            className="p-3 rounded-lg"
+            type="text"
+            placeholder="Username"
+          />
+          <input className="p-3 rounded-lg" type="email" placeholder="Email" />
+          <input
+            className="p-3 rounded-lg"
+            type="Password"
+            placeholder="Password"
+          />
+
+          <button className=" bg-slate-700 p-3 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            Sign up
+          </button>
+        </form>
+
+        <div className=" flex flex-col items-center gap-2 mt-5">
+          <p> Have an account?</p>
+          <Link to={"/sign-in"}>
+            <span className="text-blue-700">Sign in </span>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
