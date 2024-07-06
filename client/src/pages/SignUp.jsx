f2de237067e53ace24b6325c161bf739d0014cba
Abhi Mishra <sonusamda@gmail.com>
2024-07-06 12:26:48 +0530

    SingUp retouch

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,38 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <>
-      <div className=" p-3 max-w-md mx-auto">
-        <h1 className="text-center my-7 text-3xl font-semibold">Sign Up</h1>
+    <div className="p-3  max-w-md  sm:max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
 
-        <form className="flex flex-col gap-3 ">
-          <input
-            className="p-3 rounded-lg"
-            type="text"
-            placeholder="Username"
-          />
-          <input className="p-3 rounded-lg" type="email" placeholder="Email" />
-          <input
-            className="p-3 rounded-lg"
-            type="Password"
-            placeholder="Password"
-          />
-
-          <button className=" bg-slate-700 p-3 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-            Sign up
-          </button>
-        </form>
-
-        <div className=" flex flex-col items-center gap-2 mt-5">
-          <p> Have an account?</p>
-          <Link to={"/sign-in"}>
-            <span className="text-blue-700">Sign in </span>
-          </Link>
-        </div>
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign UP</button>
+     
+      </form>
+      <div className="flex gap-2 mt-5 justify-center">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign in</span>
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
