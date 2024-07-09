0da32250248150d7adc692721688aa69561fe9d6
Abhi Mishra <sonusamda@gmail.com>
2024-07-09 13:28:56 +0530

    functinality add in Signup

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,20 +8,15 @@ const Header = () => {
       <div className="flex justify-evenly items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Sahand</span>
+            <span className="text-slate-500">Abhi</span>
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form
-        
-          className="bg-slate-100 p-3 rounded-lg flex items-center"
-        >
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
-           
-          
           />
           <button>
             <FaSearch className="text-slate-600" />
@@ -38,13 +33,10 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to="/sign-in"><li>
-            Sign In
-          </li></Link>
-          <Link to="/profile">
-
-           
+          <Link to="/sign-in">
+            <li>Sign In</li>
           </Link>
+          <Link to="/profile"></Link>
         </ul>
       </div>
     </header>
