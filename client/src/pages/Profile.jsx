d6b38ca0639b6f0e2d7efd237669f7820a964081
Abhi Mishra <sonusamda@gmail.com>
2024-08-06 09:23:55 +0530

    Profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,54 +1,55 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import { useRef } from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-  const { currentUser } = useSelector(state => state.user)
+  const { currentUser } = useSelector((state) => state.user);
   
-  const handleSubmit = (first) => { second }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
 
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col">
+      
         <img
-          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
           src={currentUser.avatar}
+          className=" sm:scale-125 mt-3 object-cover rounded-full self-center"
           alt="profile"
+          
         />
 
         <input
           type="text"
           placeholder="username"
           id="username"
-          className="p-3  border rounded-lg "
+          className="gap-3 p-2 border rounded-xl mt-7"
         />
         <input
-          type="text"
+          type="email"
           placeholder="email"
-          id="email"
-          className="p-3  border rounded-lg "
+          id="username"
+          className="gap-3 p-2 border rounded-xl mt-7"
         />
         <input
-          type="text"
+          type="password"
           placeholder="password"
-          id="password"
-          className="p-3  border rounded-lg : "
+          id="username"
+          className="gap-3 p-2 border rounded-xl mt-7"
         />
 
-        <button
-          onSubmit={handleSubmit}
-          className="bg-slate-700 p-3 rounded-lg text-white uppercase"
-        >
-          Update
+        <button className="uppercase border font-semibold rounded-lg mt-5 p-3 bg-slate-600 text-white">
+          update
         </button>
-
-        <div className=' flex justify-between'>
-          <button className="text-red-700">Delete account ?</button>
-          <button className="text-red-700">Sign out</button>
+        <button className="uppercase  text-white border font-semibold rounded-lg mt-3 p-3 bg-green-600">
+          Create Listing
+        </button>
+        <div className="flex flex-row justify-between mx-3 mt-2">
+          <button className="text-red-600 text-lg ">Delete Account</button>
+          <button className="text-red-600 text-lg ">Logout</button>
         </div>
       </form>
     </div>
   );
-}
+};
 
-export default Profile
+export default Profile;
